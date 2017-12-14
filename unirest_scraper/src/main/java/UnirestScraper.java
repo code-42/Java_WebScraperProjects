0b4fe9be5166496a2bc3ecb83bf9d21938cc98b6
Edward Dupre <edwd42@gmail.com>
2017-12-14 14:59:52 -0500

    add dependencies to POM

--- unirest_scraper/src/main/java/UnirestScraper.java
+++ unirest_scraper/src/main/java/UnirestScraper.java
@@ -1,7 +1,18 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 public class UnirestScraper {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnirestException {
 
-        
+
+        // Dependencies http://unirest.io/java.html
+        //Making a simple get request to httpbin.org
+        final HttpResponse<JsonNode> getResponse = Unirest.get("http://httpbin.org/get").queryString("limit", 10)
+                .asJson();
+
+        System.out.println(getResponse.getBody());
     }
 }
