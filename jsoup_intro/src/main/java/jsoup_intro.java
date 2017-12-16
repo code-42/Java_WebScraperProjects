19a62eca4c40dc2807089d6bc2107e75f93dc3f2
Edward Dupre <edwd42@gmail.com>
2017-12-16 13:12:41 -0500

    edit comments

--- jsoup_intro/src/main/java/jsoup_intro.java
+++ jsoup_intro/src/main/java/jsoup_intro.java
@@ -19,7 +19,6 @@ public class jsoup_intro {
         for (Element headline : newsHeadlines) {
             System.out.println(headline.text());
         }
-        
         System.out.println("\n\n---------------------------------------------------\n\n");
 
         // Print the outer html of the page
