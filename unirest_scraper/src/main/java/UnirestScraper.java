0a53c87073d6657a9b8ecba6616ce64b8c0b4bb3
Edward Dupre <edwd42@gmail.com>
2017-12-22 17:18:40 -0500

    add Unirest header and getJSONObject methods

--- unirest_scraper/src/main/java/UnirestScraper.java
+++ unirest_scraper/src/main/java/UnirestScraper.java
@@ -2,19 +2,29 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.HttpRequest;
+import org.apache.http.HttpHost;
 
 public class UnirestScraper {
 
-    public static void main(String[] args) throws UnirestException {
+    public static void main(String[] args) throws Exception {
 
+
+        // Change the default user-agent header
+        Unirest.setDefaultHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36");
+
+        // Use a proxy
+//        Unirest.setProxy(new HttpHost("89.238.128.173", 80));
 
         // Dependencies http://unirest.io/java.html
         // Making a simple get request to httpbin.org
-        final HttpResponse<JsonNode> getResponse = Unirest.get("http://httpbin.org/get").queryString("limit", 10)
-                .asJson();
+//        final HttpResponse<String> getResponse = Unirest.get("http://httpbin.org/get").asString();
+        final HttpResponse<JsonNode> getResponse = Unirest.get("http://httpbin.org/get").queryString("limit", 10).asJson();
+//
+        System.out.println(getResponse.getBody());
+        System.out.println("proxy ip:      " + getResponse.getBody().getObject().getString("origin")); // returns ip address
+        System.out.println("User-Agent:    " + getResponse.getBody().getObject().getJSONObject("headers").getString("User-Agent"));
 
-        System.out.println(getResponse.getBody().getObject());
-        System.out.println(getResponse.getBody().getObject().getString("origin")); // returns ip address
     }
 
 
