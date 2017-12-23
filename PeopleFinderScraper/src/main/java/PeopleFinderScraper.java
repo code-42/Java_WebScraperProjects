6b7532dd0c197963ac76ec129645d6185785b299
Edward Dupre <edwd42@gmail.com>
2017-12-22 19:36:36 -0500

    initial commit

---
+++ PeopleFinderScraper/src/main/java/PeopleFinderScraper.java
@@ -0,0 +1,17 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PeopleFinderScraper {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
