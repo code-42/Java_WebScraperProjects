e941217f8d777890a409d8fb2fca7a9044a376e4
Edward Dupre <edwd42@gmail.com>
2017-12-23 13:16:42 -0500

    initial commit

---
+++ CoinMarketCapScraper/src/main/java/CoinMarketCapScraper.java
@@ -0,0 +1,25 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.GetRequest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.*;
+
+public class CoinMarketCapScraper {
+
+
+    public static void main(String[] args) throws Exception {
+
+
+    }
+}
