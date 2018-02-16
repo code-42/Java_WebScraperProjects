9069c8de848bd450cba35a8a781add3c8544cf27
Edward Dupre <edwd42@gmail.com>
2018-02-15 23:33:53 -0500

    probing html doc forlinks, images and forms attributes

--- OptioinsScraper/src/net/ed/OptionsScraper.java
+++ OptioinsScraper/src/net/ed/OptionsScraper.java
@@ -1,28 +1,26 @@
 package net.ed;
 
-import java.io.PrintWriter;
+/**
+ * OptionsScraper project
+ * created by Edward Dupre
+ * copyright 2018
+ * scrape data from
+ * https://oic.ivolatility.com/oic_adv_options.j;jsessionid=aC-rWaKSNTra
+ *
+ * much of this page learned from https://aboullaite.me/jsoup-html-parser-tutorial-examples/
+ *
+ */
 
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.mashape.unirest.http.HttpResponse;
-//import com.mashape.unirest.http.Unirest;
-//import com.mashape.unirest.http.exceptions.UnirestException;
-//import com.mashape.unirest.request.GetRequest;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.safety.Cleaner;
+import org.jsoup.safety.Whitelist;
 import org.jsoup.select.Elements;
-
-import javax.lang.model.util.ElementScanner6;
-//import javax.swing.text.Document;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.reflect.Array;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.regex.*;
 
 public class OptionsScraper {
@@ -33,39 +31,96 @@ public class OptionsScraper {
 
     public static void main(String[] args) throws Exception {
 
-        String url = "https://coinmarketcap.com/all/views/all/";
+        String url = "https://oic.ivolatility.com/oic_adv_options.j;jsessionid=aC-rWaKSNTra";
         String query = "";
 
-        // Downloads the html from coinmarketcap.com and parses it
-        final org.jsoup.nodes.Document doc = Jsoup.connect(url + URLEncoder.encode(query,"UTF-8"))
-                .userAgent(USER_AGENT)
-                .timeout(20000)
-                .maxBodySize(0)
-                .get();
+        // Downloads the html from oic.volatility.com and parses it
+//        final org.jsoup.nodes.Document doc = Jsoup.connect(url + URLEncoder.encode(query,"UTF-8"))
+//                .userAgent(USER_AGENT)
+//                .timeout(20000)
+//                .maxBodySize(0)
+//                .get();
 
-        PrintWriter csv = new PrintWriter("results.csv");
+        String doc = url;
+        String html = Jsoup.connect(doc).get().html();
+        System.out.println(html);
 
-        // tbody is first container inside of the first table
-        Element table = doc.select("tbody").get(0);
+        try {
 
-        // Target each row in tbody
-        Elements rows = table.select("tr");
+            Document page = Jsoup.connect(url).get();
+//            String description = doc.select("meta[name=description]").first().attr("content");
+//            System.out.println("Description: " + description);
+//
+//            String keywords = doc.select("meta[name=keywords]").first().attr("content");
+//            System.out.println("Keywords: " + keywords);
 
-        // Target each td inside of each row
-        for (int r = 0; r < rows.size(); r++) {
-            Element row = rows.get(r);
-            Elements cols = row.select("td");
-
-            // print td for each row
-            for (int c = 0; c < cols.size(); c++) {
-                System.out.print(cols.get(c).text() + "\t");
-                csv.write((cols.get(c).text() + "\t"));
+            // get links on page
+            Elements links = page.select("a[href]");
+            for (Element link : links){
+                System.out.println("link: " + link.attr("href"));
+                System.out.println("text: " + link.text());
             }
 
-            // empty println is same as \n
-            System.out.println();
-            csv.write("\n");
+
         }
-        csv.close();
+        catch (NullPointerException e){
+            System.out.println("errr: " + e);
+        }
+
+
+        // sanitizing html
+        boolean valid = Jsoup.isValid(doc, Whitelist.basic());
+
+        if (valid) {
+
+            System.out.println("\n!!!!! The document is valid");
+        } else {
+
+            System.out.println("\n!!!!! The document is not valid.");
+            System.out.println("Cleaned document");
+
+            Document dirtyDoc = Jsoup.parse(doc);
+            Document cleanDoc = new Cleaner(Whitelist.basic()).clean(dirtyDoc);
+
+            System.out.println(cleanDoc.html());
+        }
+
+        // get forms attributes
+        try {
+
+            Document docx = Jsoup.connect(url).get();
+            Elements formElements = docx.getElementsByTag("form");
+
+            for (Element formElement : formElements) {
+                String key = formElement.attr("name");
+                String value = formElement.attr("action");
+                System.out.println("Param name: " + key + " \nParam value: " + value);
+            }
+        }
+        catch (NullPointerException e){
+            System.out.println("errr: " + e);
+        }
+
+
+        // get images
+//        Document doc;
+        try {
+
+            //get all images
+            Document doc2 = Jsoup.connect(url).get();
+            Elements images = doc2.select("img[src~=(?i)\\.(png|jpe?g|gif)]");
+            for (Element image : images) {
+
+                System.out.println("\nsrc : " + image.attr("src"));
+                System.out.println("height : " + image.attr("height"));
+                System.out.println("width : " + image.attr("width"));
+                System.out.println("alt : " + image.attr("alt"));
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
